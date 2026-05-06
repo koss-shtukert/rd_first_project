ec46b8e458e0e85cb55f921fdd37002eb7bddeae
Kostiantyn Shtukert <k.shtukert@foodtech.team>
2026-05-06 09:49:51 +0300

    feat-FLAB-12-widgets-p2: Task 1 - Add missing

---
+++ lib/lesson_12/constants/assets.dart
@@ -0,0 +1,8 @@
+abstract class Lesson12Assets {
+  static const ratingFilled = 'assets/icons/rating.png';
+  static const ratingEmpty = 'assets/icons/rating_empty.png';
+  static const likeActive = 'assets/icons/like_active.png';
+  static const likeDefault = 'assets/icons/like.png';
+  static const dislikeActive = 'assets/icons/dislike_active.png';
+  static const dislikeDefault = 'assets/icons/dislike.png';
+}
